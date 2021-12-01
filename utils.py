8b5fb4dce7fa2fc2ed6e2d3062b52b973bd3e259
hutli <jensstaermose@hotmail.com>
2021-12-01 21:02:59 +0100

    Better day data loading

--- utils.py
+++ utils.py
@@ -6,15 +6,12 @@ import requests
 dotenv.load_dotenv()
 
 
-def get_todays_input(day):
-    return numpy.array(
-        [
-            int(s)
-            for s in requests.get(
-                f"https://adventofcode.com/2021/day/{day}/input",
-                cookies={"session": os.environ["SESSION"]},
-            )
-            .text.strip()
-            .split("\n")
-        ]
+def get_todays_input(day, dtype=numpy.int32, sep="\n"):
+    return numpy.fromstring(
+        requests.get(
+            f"https://adventofcode.com/2021/day/{day}/input",
+            cookies={"session": os.environ["SESSION"]},
+        ).text,
+        dtype=dtype,
+        sep=sep,
     )
